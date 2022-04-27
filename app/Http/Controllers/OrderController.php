77a1b69828f365c6c62f88995cc8e428615f42eb
Vishwanath <vishwanath.pucha@conv.in>
2022-04-27 23:46:52 +0530

    first commit

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreOrderRequest;
+use App\Interfaces\OrderRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Request;
+
+class OrderController extends Controller
+{
+    private OrderRepositoryInterface $orderRepository;
+
+    public function __construct(OrderRepositoryInterface $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'data' => $this->orderRepository->getAllOrders()
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOrderRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOrderRequest $request): JsonResponse
+    {
+        $orderDetails = $request->only([
+            'client', 'details'
+        ]);
+
+        return response()->json(
+            [
+                'data' => $this->orderRepository->createOrder($orderDetails)
+            ],
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function show($orderId): JsonResponse
+    {
+        $orderId = $orderId;
+
+        return response()->json([
+            'data' => $this->orderRepository->getOrderById($orderId)
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOrderRequest  $request
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request): JsonResponse
+    {
+        $orderId = $request->route('id');
+        $orderDetails = $request->only([
+            'client',
+            'details'
+        ]);
+
+        return response()->json([
+            'data' => $this->orderRepository->updateOrder($orderId, $orderDetails)
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        $orderId = $request->route('id');
+        $this->orderRepository->deleteOrder($orderId);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
